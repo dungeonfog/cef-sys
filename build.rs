4454559343668bb4f712d92d560e6c39a6d4c03b
Andreas Monitzer <andreas@monitzer.com>
2019-08-26 17:04:49 +0200

    Initial commit

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+fn main() {
+    let target_os = env::var("CARGO_CFG_TARGET_OS");
+
+    match target_os.as_ref().map(|x| &**x) {
+        Ok("windows") => {
+            println!("cargo:rustc-link-lib=cef_sandbox");
+            println!("cargo:rustc-link-lib=libcef");
+        },
+        Ok("linux") => {
+            println!("cargo:rustc-link-lib=cef");
+            println!("cargo:rustc-link-lib=EGL");
+            println!("cargo:rustc-link-lib=GLESv2");
+        }
+        _ => {},
+    }
+}
