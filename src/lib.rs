02dec9a17d2b434a1f49dc8077397e825eb5f5e4
Andreas Monitzer <andreas@monitzer.com>
2019-08-26 23:44:13 +0200

    Should have tested compiling on Linux as well.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-#[cfg(windows)]
+#[cfg(target_os = "windows")]
 mod bindings_windows;
-#[cfg(windows)]
+#[cfg(target_os = "windows")]
 pub use bindings_windows::*;
 
-#[cfg(linux)]
+#[cfg(target_os = "linux")]
 mod bindings_linux;
-#[cfg(linux)]
+#[cfg(target_os = "linux")]
 pub use bindings_linux::*;
