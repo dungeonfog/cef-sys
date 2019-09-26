b8e4646941f32fd9a500e4ca8cfa28c45b8de127
Andreas Monitzer <andreas@monitzer.com>
2019-09-26 15:38:29 +0200

    Added a missing header file.

--- wrapper.hpp
+++ wrapper.hpp
@@ -3,5 +3,6 @@
 #include "include/capi/cef_client_capi.h"
 #include "include/capi/cef_urlrequest_capi.h"
 #include "include/capi/cef_life_span_handler_capi.h"
+#include "include/capi/cef_web_plugin_capi.h"
 #include "include/cef_pack_resources.h"
 #include "include/cef_pack_strings.h"
