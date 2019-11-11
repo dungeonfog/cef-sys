687f158835ec4e4364a2b93c158d52cbbc16382a
Osspial <osspial@gmail.com>
2019-11-11 17:46:14 -0500

    Add misc. generation files to source tree.
    These aren't going to build in their current state, but they'll be
    useful when generating new bindings.

---
+++ generator/main.rs
@@ -0,0 +1,17 @@
+use bindgen::builder;
+fn main() {
+    let mut bindings = builder()
+        .header("S:/cef/git/chromium/src/everything.h")
+        .whitelist_type("_?cef_.*")
+        .whitelist_function("_?cef_.*")
+        .whitelist_var("_?cef_.*")
+        .default_enum_style(bindgen::EnumVariation::ModuleConsts)
+        .bitfield_enum("cef_transition_type_t")
+        .bitfield_enum("cef_v8_propertyattribute_t")
+        .bitfield_enum("cef_v8_accesscontrol_t")
+        .bitfield_enum("cef_drag_operations_mask_t")
+        .bitfield_enum("cef_cert_status_t")
+        .bitfield_enum("cef_file_dialog_mode_t")
+        .bitfield_enum(".+flags.+");
+    bindings.generate().unwrap().write_to_file("./bindings.rs").unwrap();
+}
