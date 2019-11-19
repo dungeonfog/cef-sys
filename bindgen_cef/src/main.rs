c485974da04ccdd0274b27932be28a7085678d56
Osspial <osspial@gmail.com>
2019-11-19 14:59:07 -0500

    Make bindgen_cef work on Linux

--- bindgen_cef/src/main.rs
+++ bindgen_cef/src/main.rs
@@ -1,7 +1,7 @@
 use bindgen::builder;
 use std::env;
 use std::iter;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 fn main() {
     let chromium_path = env::args()
         .skip(1)
@@ -16,15 +16,19 @@ fn main() {
     ];
     #[cfg(target_os = "windows")]
     let include = "INCLUDE";
+    #[cfg(target_os = "windows")]
+    let separator = ";";
     #[cfg(not(target_os = "windows"))]
     let include = "C_INCLUDE_PATH";
+    #[cfg(not(target_os = "windows"))]
+    let separator = ":";
 
     env::set_var(
         include,
         include_paths
             .into_iter()
             .chain(iter::once(env::var(include).unwrap_or(String::new())))
-            .map(|s| s + ";")
+            .map(|s| s + separator)
             .collect::<String>(),
     );
 
@@ -34,13 +38,18 @@ fn main() {
         .whitelist_function("_?cef_.*")
         .whitelist_var("_?cef_.*")
         .default_enum_style(bindgen::EnumVariation::ModuleConsts)
-        .bitfield_enum("cef_transition_type_t")
-        .bitfield_enum("cef_v8_propertyattribute_t")
-        .bitfield_enum("cef_v8_accesscontrol_t")
-        .bitfield_enum("cef_drag_operations_mask_t")
         .bitfield_enum("cef_cert_status_t")
-        .bitfield_enum("cef_file_dialog_mode_t")
-        .bitfield_enum(".+flags.+")
+        .bitfield_enum("cef_v8_propertyattribute_t")
+        .bitfield_enum("cef_urlrequest_flags_t")
+        .bitfield_enum("cef_event_flags_t")
+        .bitfield_enum("cef_context_menu_type_flags_t")
+        .bitfield_enum("cef_context_menu_media_state_flags_t")
+        .bitfield_enum("cef_context_menu_edit_state_flags_t")
+        .bitfield_enum("cef_uri_unescape_rule_t")
+        .bitfield_enum("cef_json_parser_options_t")
+        .bitfield_enum("cef_json_writer_options_t")
+        .bitfield_enum("cef_ssl_content_status_t")
+        .bitfield_enum("cef_scheme_options_t")
         .generate()
         .unwrap();
     bindings.write_to_file("./bindings.rs").unwrap();
